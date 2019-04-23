5734d4685b09fc34838a57d59070052aa1e0cfd5
unknown <mr.danieljberg@gmail.com>
2019-04-23 11:26:41 -0500

    Added distance to my driver and approach clubs

--- diceGame.js
+++ diceGame.js
@@ -1,33 +1,21 @@
-let driverRough = 3
-let driverFairway = 15
-let approachMiss = 1
-let approachGreen = 6
-let missedShot = 2
-let holeNumber = 1
+
+// what numbers mean inside array [value to miss right of left, dice side value, min distance, max distance]
+let driver = [3,15,225,300];
+let approach = [1,6,100,200];
+
+
+
+let missedShot = 2;
+let holeNumber = 1;
 
 function rollDie(sides){
 	let myRoll = Math.floor((Math.random() * sides) + 1);
 	return myRoll
 }
 function rollDieDistance(min , max){
-	let myRoll = Math.floor(Math.random() * (min - max) ) + min;
+	let myRoll = Math.floor(Math.random() * (max - min + 1) ) + min;
 	return myRoll
 }
-console.log(rollDieDistance(225,300))
-
-/*
-let shotDirection = Math.floor((Math.random() * 5) + 1);
-let shotDirectionPutter = Math.floor((Math.random() * 6) + 1);
-let shotLengthDriver = Math.floor((Math.random() * 20) + 1);
-let shotLengthApproachIron = Math.floor((Math.random() * 10) + 1);
-let shotLengthWedge = Math.floor((Math.random() * 5) + 1);
-let shotLengthPutter = Math.floor((Math.random() * 4) + 1);
-*/
-
-
-console.log("Welcome to Hole #" + holeNumber);
-console.log("Grab your club and step up to the tee box");
-
 function ballLieFirstShot(rough , fairway){
 	let myRoll = rollDie(fairway);
 	console.log(myRoll);
@@ -43,10 +31,7 @@ function ballLieFirstShot(rough , fairway){
 		let ballLie = "Fairway";
 		return ballLie;
 	}
-
 }
-
-
 function ballLieSecondShot(miss , green){
 	if (myBall === "Left Rough" || myBall === "Right Rough"){
 		green = green - missedShot;
@@ -69,32 +54,31 @@ function ballLieSecondShot(miss , green){
 		let myRoll = rollDie(green);
 		console.log(myRoll);
 		if (myRoll == miss){
-			let ballLie = "Missed Green Left";
+			let ballLie = "Missed The Green Left";
 			return ballLie;
 		}
 		else if (myRoll == green){
-			let ballLie = "Missed Green Right";
+			let ballLie = "Missed The Green Right";
 			return ballLie;
 		}
 		else {
-			let ballLie = "Hit Green";
+			let ballLie = "Hit The Green";
 			return ballLie;
 		}
 	}
 }
+//function puttBall()
+
+console.log("Welcome to Hole #" + holeNumber);
+console.log("Grab your club and step up to the tee box");
+
+let myBall = ballLieFirstShot(driver[0],driver[1]);
+console.log("You hit the ball " + rollDieDistance(driver[2], driver[3]) + "yrds in the " + myBall);
+
+myBall = ballLieSecondShot(approach[0],approach[1]);
+console.log("On your second hit you hit the ball " + rollDieDistance(approach[2], approach[3]) + "yrds and " + myBall);
+
+//if (myBall === "Hit Green"){
 
 
-
-function ballDistance(club){
-	
-	let myRoll = rollDie(club)
-}
-
-
-
-let myBall = ballLieFirstShot(driverRough,driverFairway);
-console.log("You hit the ball in the " + myBall);
-
-myBall = ballLieSecondShot(approachMiss,approachGreen);
-console.log("On your second hit you " + myBall);
 
